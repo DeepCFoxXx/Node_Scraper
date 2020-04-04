18ab8c5afbce51305a004e98a29a0ca784a0bedd
Mark <learncode23@gmail.com>
2020-04-04 12:36:04 +0100

    Further front-end structure added

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,11 @@
+import Page from "../components/Page";
+import Data from "../components/Data";
+
 export default function Home() {
-  return <p>Hiya!</p>;
+  return (
+    <Page>
+      <p>Home Page</p>
+      <Data />
+    </Page>
+  );
 }
